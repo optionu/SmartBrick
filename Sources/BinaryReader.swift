e8996aa7641a33077edad72e09d2ab9377ca5abb
Claus Höfele <claus@claushoefele.com>
2017-04-05 22:38:10 +0200

    Add BinaryReader

---
+++ Sources/BinaryReader.swift
@@ -0,0 +1,35 @@
+//
+//  BinaryReader.swift
+//  SmartBricks
+//
+//  Created by Claus Höfele on 05.04.17.
+//  Copyright © 2017 SmartBricks. All rights reserved.
+//
+
+import Foundation
+
+class BinaryReader {
+    var position = 0
+    let data: Data
+    let bigEndian: Bool
+    
+    init(withData data: Data, bigEndian: Bool = false) {
+        self.data = data
+        self.bigEndian = bigEndian
+    }
+    
+    func readUInt32() -> UInt32 {
+        let value: UInt32 = readInteger(fromData: data, start: position)
+        position += MemoryLayout<UInt32>.size
+        
+        return bigEndian ? UInt32(bigEndian: value) : UInt32(littleEndian: value)
+    }
+
+    private func readInteger<T: Integer>(fromData data: Data, start: Int) -> T {
+        return data.withUnsafeBytes({(bytePointer: UnsafePointer<UInt8>) in
+            bytePointer.advanced(by: start).withMemoryRebound(to: T.self, capacity: MemoryLayout<T>.size) { pointer in
+                return pointer.pointee
+            }
+        })
+    }
+}
---
+++ Tests/SmartBricksTests/BinaryReaderTests.swift
@@ -0,0 +1,41 @@
+//
+//  BinaryReaderTests.swift
+//  SmartBricks
+//
+//  Created by Claus Höfele on 05.04.17.
+//  Copyright © 2017 SmartBricks. All rights reserved.
+//
+
+import XCTest
+@testable import SmartBricks
+
+class BinaryReaderTests: XCTestCase {
+    func testBigEndian() {
+        let bigEndianData = Data(bytes: [0x01, 0x02, 0x03, 0x04])
+        let binaryReader = BinaryReader(withData: bigEndianData, bigEndian: true)
+
+        XCTAssertEqual(binaryReader.readUInt32(), 16909060)
+    }
+    
+    func testLittleEndian() {
+        let bigEndianData = Data(bytes: [0x04, 0x03, 0x02, 0x01])
+        let binaryReader = BinaryReader(withData: bigEndianData, bigEndian: false)
+        
+        XCTAssertEqual(binaryReader.readUInt32(), 16909060)
+    }
+    
+    // canReadNumberOfBytes
+    // skipNumberOfBytes
+    // readUInt8
+    // readUInt16
+}
+
+#if os(Linux)
+    extension SmartBricksTests {
+        static var allTests : [(String, (SmartBricksTests) -> () throws -> Void)] {
+            return [
+                ("testExample", testExample),
+            ]
+        }
+    }
+#endif
